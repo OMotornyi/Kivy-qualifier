52b8c37de7910b7a203921ccb81fb0ae729b0070
OMotornyi <motorny.sasha@gmail.com>
2020-01-10 02:34:13 +0100

    Added a slider for zoom step

--- qualifier.py
+++ qualifier.py
@@ -1,13 +1,6 @@
-"""This is a basic "Hello world" using Kivy.
-
-It uses the Builder to load a KVlang string, that describe a widget
-tree, here containing only one element, the root, a Label displaying
-"Hello world!"
-"""
-
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
-from kivy.properties import ObjectProperty, StringProperty
+from kivy.properties import ObjectProperty, NumericProperty
 from kivy.core.window import Window
 from kivy.uix.scatter import Scatter
 from kivy.uix.popup import Popup
@@ -40,41 +33,35 @@ from pathlib import Path
 # call, so it can be added to the widget tree, either as root, or to an
 # existing parent widget of your choice. In the example below, "Label"
 # is the "root rule".
-class PydisLogo(Scatter):
-    # pass
-    def on_transform_with_touch(self, touch):
-        if self.top > self.parent.top:
-            self.top = self.parent.top
-            print("COLLIDE!!!!!")
-        if self.y < self.parent.y:
-            self.y = self.parent.y
-        if self.right > self.parent.right:
-            self.right = self.parent.right
-            print("COLLIDE!!!!!")
-        if self.x < self.parent.x:
-            self.x = self.parent.x
-    # source = StringProperty(None)
-    # def __init__(self, **kwargs):
-    #    super(PydisLogo, self).__init__(**kwargs)
-    #   print(self.parent)
 
-    #     self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
-    #     self._keyboard.bind(on_key_down=self._on_keyboard_down)
-    #
-    # def _keyboard_closed(self):
-    #     self._keyboard.unbind(on_key_down=self._on_keyboard_down)
-    #     self._keyboard = None
-    #
-    # def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
-    #     if keycode[1] == 'left':
-    #         print("Moving left")
-    #         self.x -= 100
-    #     elif keycode[1] == 'right':
-    #         self.x += 10
-    #     elif keycode[1] == 'up':
-    #         self.center_y += 10
-    #     elif keycode[1] == 'down':
-    #         self.center_y -= 10
+def _zoom(scat,img,factor):
+    scat.scale*=factor
+    # center = (scat.center_x, scat.center_y)
+    # img.height *= factor
+    # img.width *= factor
+    # scat.center_x = center[0]
+    # scat.center_y = center[1]
+
+def _position_within_window(scat,frame):
+    if scat.top > frame.top:
+        scat.top = frame.top
+    if scat.top > frame.load_button.y:
+        scat.top = frame.load_button.y
+    if scat.y < frame.y:
+        scat.y = frame.y
+    if scat.right > frame.right:
+        scat.right = frame.right
+    if scat.x < frame.x:
+        scat.x = frame.x
+
+
+class PydisLogo(Scatter):
+    def on_transform_with_touch(self, touch):
+        _position_within_window(self, self.parent)
+    def reset(self):
+        print("RESET")
+        print(self.rotation)
+        print(self.scale)
 
 
 class Root(FloatLayout):
@@ -82,8 +69,8 @@ class Root(FloatLayout):
     scat = ObjectProperty(None)
     pic = ObjectProperty(None)
     loadfile = ObjectProperty(None)
-
-    # source = "data/python_discord_logo.png"
+    load_button = ObjectProperty(None)
+    zoom_factor = NumericProperty(1.1)
 
     def __init__(self, **kwargs):
         super(Root, self).__init__(**kwargs)
@@ -95,9 +82,7 @@ class Root(FloatLayout):
         self._keyboard = None
 
     def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
-        print(self.pic)
         if keycode[1] == 'left':
-            print("Moving left")
             self.scat.x -= 10
         elif keycode[1] == 'right':
             self.scat.x += 10
@@ -106,26 +91,12 @@ class Root(FloatLayout):
         elif keycode[1] == 'down':
             self.scat.y -= 10
         elif keycode[1] == 'w':
-            center = (self.scat.center_x, self.scat.center_y)
-            self.img.width *= 1.1
-            self.img.height *= 1.1
-            self.scat.center_x = center[0]
-            self.scat.center_y = center[1]
+            #_zoom(self.scat,self.img, self.zoom_factor)
+            _zoom(self.scat, self.scat, self.zoom_factor)
         elif keycode[1] == 's':
-            center = (self.scat.center_x, self.scat.center_y)
-            self.img.height *= 0.9
-            self.img.width *= 0.9
-            self.scat.center_x = center[0]
-            self.scat.center_y = center[1]
-        if self.scat.top > self.top:
-            self.scat.top = self.top
-        if self.scat.y < self.y:
-            self.scat.y = self.y
-        if self.scat.right > self.right:
-            self.scat.right = self.right
-#            print("COLLIDE!!!!!")
-        if self.scat.x < self.x:
-            self.scat.x = self.x
+            #_zoom(self.scat,self.img, 1/self.zoom_factor)
+            _zoom(self.scat, self.scat, 1/self.zoom_factor)
+        _position_within_window(self.scat, self)
 
     def show_load(self):
         content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
@@ -137,20 +108,19 @@ class Root(FloatLayout):
         self._popup.dismiss()
 
     def load(self, path, filename):
-        print(f"Path: '{path}', Filename: '{filename}'")
-        # self.source = filename
         self.img.source = filename[0]
         self.img.reload()
         self.dismiss_popup()
 
 
+
 class LoadDialog(FloatLayout):
     load = ObjectProperty(None)
     cancel = ObjectProperty(None)
-    home = StringProperty(str(Path.home()))
+    home = str(Path.home())#StringProperty(str(Path.home()))
 
 
-class ImageMover(App):
+class Application(App):
     """The application class manages the lifecycle of your program, it
     has events like on_start, on_stop, etc.
 
@@ -175,4 +145,4 @@ class ImageMover(App):
 if __name__ == "__main__":
     # calling run method of the application will build the widget tree,
     # and start the event loop.
-    ImageMover().run()
+    Application().run()
